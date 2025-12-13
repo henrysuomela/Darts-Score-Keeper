4f0e090eb2996f6661250a5c7012f14d46a2696a
Henry Suomela <henry.suomela@edu.turkuamk.fi>
2025-12-13 15:39:20 +0200

    kommentit lisätty

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+// Funktiot kiinni nappeihin
 const startButton = document.getElementById("startButton");
 startButton.addEventListener("click", startGame);
 
 const submitScoreButton = document.getElementById("submitScoreButton");
 submitScoreButton.addEventListener("click", throwDart);
 
+// Submit nappi pois käytöstä jos peli ei oo alkanut vielä
 submitScoreButton.disabled = true;
 
 function addPlayer(name) {
@@ -42,11 +44,13 @@ const legOverMessage = document.getElementById("legOverMessage");
 
 function startGame() {
 
+    // Viestit tyhjäks jos jääny edellisestä pelistä
     scoreForThrowMessage.textContent = gameOverMessage.textContent = legOverMessage.textContent = "";
 
     const player1Name = document.getElementById("player1Name").value;
     const player2Name = document.getElementById("player2Name").value;
 
+    // Jos nimiä ei oo syötetty, defaultataan Player 1 ja Player 2
     player1 = addPlayer(player1Name || "Player 1");
     player2 = addPlayer(player2Name || "Player 2");
 
@@ -55,12 +59,15 @@ function startGame() {
     const setSize = parseInt(document.getElementById("setSize").value);
     const setSizeErrorMessage = document.getElementById("setSizeErrorMessage");
     const winningLegsMessage = document.getElementById("winningLegsMessage");
+    // Tsekataan et setSize on pariton luku
     if (setSize % 2 === 0 || isNaN(setSize)) {
         setSizeErrorMessage.textContent = "Set size must be an odd number.";
         return;
     }
     else {
+        // Error message tyhjäks kun pariton luku syötetty
         setSizeErrorMessage.textContent = "";
+        // Määritellään vaadittavat leg voitot
         winningLegs = setSize / 2 + 0.5;
         winningLegsMessage.textContent = "Legs required to win: " + winningLegs;
     }
@@ -72,6 +79,7 @@ function startGame() {
     throwsThisTurn = 0;
     turnMessage.textContent = "Current turn: " + currentPlayer.name + ", throws: " + throwsThisTurn;
 
+    // Heiton submit nappi käyttöön kun peli alotettu
     submitScoreButton.disabled = false;
 
 }
@@ -83,10 +91,12 @@ function throwDart() {
 
     const scoreErrorMsg = document.getElementById("scoreInputErrorMessage")
 
+    // Jos legi päättynyt, viesti siitä tyhjäks kun heitetään seuraavaks
     if (legOverMessage.textContent) {
         legOverMessage.textContent = "";
     }
 
+    // Kaikki luvut 0-60 väliltä ei oo mahollisia, lista mahollisista pisteistä
     const possiblePoints = [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
         11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
@@ -95,53 +105,67 @@ function throwDart() {
     ];
 
     const points = parseInt(document.getElementById("scoreInput").value);
+    // Heiton pisteiden pitää olla numero
     if (isNaN(points)) {
         scoreErrorMsg.textContent = "Enter a number.";
         scoreForThrowMessage.textContent = "";
         return;
     }
+    // Pitää olla joku mahdollisista pistemääristä
     else if (!possiblePoints.includes(points)) {
         scoreErrorMsg.textContent = points + " isn't a valid score for a throw.";
         scoreForThrowMessage.textContent = "";
         return;
     }
+    // Error message tyhjäks kun sopiva luku syötetty
     else {
         scoreErrorMsg.textContent = "";
     }
 
+    // Otetaan pisteet vuoron alussa muistiin mahollista bustausta varten
     if (throwsThisTurn === 0) {
         scoreBeforeThisTurn = currentPlayer.points;
     }
     
+    // Heitetyt pisteet pois pelaajan pisteistä
     currentPlayer.points -= points;
 
+    // Bustaus
     if (currentPlayer.points < 0 || currentPlayer.points === 1) {
+        // Pisteet resettaa
         currentPlayer.points = scoreBeforeThisTurn;
         scoreForThrowMessage.textContent = currentPlayer.name + " busts, score reverts to " + scoreBeforeThisTurn + " and turn ends.";
         updateScoreboard(player1, player2);
 
+        // Vuoro vaihtuu
         throwsThisTurn = 0;
         currentPlayer = (currentPlayer === player1) ? player2 : player1;
         turnMessage.textContent = "Current turn: " + currentPlayer.name + ", throws: " + throwsThisTurn;
         return;
     }
 
+    // Jos ei bustannu, heitetyt pisteet näkyviin
     scoreForThrowMessage.textContent = currentPlayer.name + " scores " + points;
 
+    // Voitto
     if (currentPlayer.points === 0) {
         currentPlayer.legsWon += 1;
         updateScoreboard(player1, player2);
 
+        // Pelivoitto
         if (currentPlayer.legsWon === winningLegs) {
             gameOverMessage.textContent = currentPlayer.name + " wins the game with " + currentPlayer.legsWon + " leg wins!";
+            // Peli päättyy, submit nappi taas pois käytöstä
             submitScoreButton.disabled = true;
             return;
         }
+        // Legivoitto
         else {
             player1.points = player2.points = gameType;
             legOverMessage.textContent = currentPlayer.name + " wins this leg!";
             throwsThisTurn = 0;
 
+            // Vuoro vaihtuu
             currentPlayer = (currentPlayer === player1) ? player2 : player1;
             turnMessage.textContent = "Current turn: " + currentPlayer.name + ", throws: " + throwsThisTurn;
             updateScoreboard(player1, player2);
@@ -154,6 +178,7 @@ function throwDart() {
     throwsThisTurn += 1;
     turnMessage.textContent = "Current turn: " + currentPlayer.name  + ", throws: " + throwsThisTurn;
 
+    // Vuoro vaihtuu kolmen heiton jälkeen
     if (throwsThisTurn === 3) {
         throwsThisTurn = 0;
         currentPlayer = (currentPlayer === player1) ? player2 : player1;
